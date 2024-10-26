aa86071d960a862f061166f65bb1dfcc01b075da
msg8065 <msg8065@nyu.edu>
2024-10-26 22:21:51 +0530

    Create subnet.py

---
+++ subnet.py
@@ -0,0 +1,59 @@
+Problem 1: IP Address: 146.129.114.192, CIDR: 25
+Network Address: 146.129.114.192
+First Usable IP Address: 146.129.114.193
+Last Usable IP Address: 146.129.114.254
+Broadcast Address: 146.129.114.255
+
+Problem 2: IP Address: 57.114.76.234, CIDR: 28
+Network Address: 57.114.76.224
+First Usable IP Address: 57.114.76.225
+Last Usable IP Address: 57.114.76.238
+Broadcast Address: 57.114.76.239
+
+Problem 3: IP Address: 224.99.37.21, CIDR: 26
+Network Address: 224.99.37.0
+First Usable IP Address: 224.99.37.1
+Last Usable IP Address: 224.99.37.62
+Broadcast Address: 224.99.37.63
+
+Problem 4: IP Address: 135.85.255.63, CIDR: 29
+Network Address: 135.85.255.56
+First Usable IP Address: 135.85.255.57
+Last Usable IP Address: 135.85.255.62
+Broadcast Address: 135.85.255.63
+
+Problem 5: IP Address: 46.70.217.106, CIDR: 27
+Network Address: 46.70.217.96
+First Usable IP Address: 46.70.217.97
+Last Usable IP Address: 46.70.217.126
+Broadcast Address: 46.70.217.127
+
+Problem 6: IP Address: 213.55.179.148, CIDR: 24
+Network Address: 213.55.179.0
+First Usable IP Address: 213.55.179.1
+Last Usable IP Address: 213.55.179.254
+Broadcast Address: 213.55.179.255
+
+Problem 7: IP Address: 124.40.140.191, CIDR: 28
+Network Address: 124.40.140.176
+First Usable IP Address: 124.40.140.177
+Last Usable IP Address: 124.40.140.190
+Broadcast Address: 124.40.140.191
+
+Problem 8: IP Address: 35.25.102.233, CIDR: 25
+Network Address: 35.25.102.128
+First Usable IP Address: 35.25.102.129
+Last Usable IP Address: 35.25.102.254
+Broadcast Address: 35.25.102.255
+
+Problem 9: IP Address: 202.10.64.19, CIDR: 29
+Network Address: 202.10.64.16
+First Usable IP Address: 202.10.64.17
+Last Usable IP Address: 202.10.64.22
+Broadcast Address: 202.10.64.23
+
+Problem 10: IP Address: 113.251.26.62, CIDR: 26
+Network Address: 113.251.26.0
+First Usable IP Address: 113.251.26.1
+Last Usable IP Address: 113.251.26.62
+Broadcast Address: 113.251.26.63
